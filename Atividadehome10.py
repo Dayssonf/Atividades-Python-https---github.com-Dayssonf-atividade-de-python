4a32eb18e90d5fa8ec2727657a26877ae5a25967
dayssonf <dayssonf@icloud.com>
2026-05-13 15:30:22 -0300

    Mais um exercícios if/elif e else feito sozinho

---
+++ Atividadehome10.py
@@ -0,0 +1,18 @@
+semana = int(input("DIgite um número de 1 a 7:"))
+
+if semana == 1:
+    print("Domingo")
+elif semana ==2:
+    print("Segunda-feira")
+elif semana ==3:
+    print("Terça feira")
+elif semana ==4:
+    print("Quarta feira")
+elif semana ==5:
+    print("Quinta feira")
+elif semana ==6:
+    print("Sexta feira")
+elif semana ==7:
+    print("Sábado")
+else:
+    print("Número inválido")
