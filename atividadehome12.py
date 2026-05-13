387374095656dfebf1c6b19a304bee486ada1cb0
dayssonf <dayssonf@icloud.com>
2026-05-13 16:12:12 -0300

    IMC

---
+++ atividadehome12.py
@@ -0,0 +1,16 @@
+peso = float(input("Digite o seu peso: "))
+altura = float(input("Digite a sua altura: "))
+
+imc = peso / (altura * altura)
+
+if imc < 18.5:
+    print("Abaixo do peso")
+
+elif imc <25:
+    print("Peso normal")
+
+elif imc <30:
+    print("Sobrepeso")
+
+else:
+    print("Obesidade")
